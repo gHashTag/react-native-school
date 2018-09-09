c47c35cb2f52d75ad132ed2ae19cc893ef843b91
Dimka Vasilyev <raoffonom@icloud.com>
2018-09-09 18:31:38 +0300

    es-lint

--- pages/WelcomePage.config.js
+++ pages/WelcomePage.config.js
@@ -1,56 +1,24 @@
-import React from 'react';
-import Header from '../views/Header';
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
 
 const Welcome = () => (
-    <div>
-        <Header/>
-        <div style={{margin: 'auto', maxWidth: 500}}>
-            <h2 style={{fontWeight: 'normal'}}>
-                Welcome to Reframe
-            </h2>
-
-            <Text style={{ fontSize: 80, textAlign: 'right' }} >
-              Home 
-            </Text>
-
-
-            Interactive pages:
-            <ul>
-                <li>
-                    <a href="/counter">/counter</a> - A counter.
-                </li>
-                <li>
-                    <a href="/time">/time</a> - Display time.
-                </li>
-            </ul>
-
-            Non-interactive pages:
-            <ul>
-                <li>
-                    <a href="/game-of-thrones">/game-of-thrones</a> - Load and display list of GoT characters.
-                </li>
-                <li>
-                    <a href="/">/</a> - This landing page.
-                </li>
-            </ul>
-
-            Route defined with Hapi:
-            <ul>
-                <li>
-                    <a href="/hello-from-hapi">/hello-from-hapi</a>
-                </li>
-            </ul>
-
-        </div>
+  <div>
+    <div style={{margin: 'auto', maxWidth: 500}}>
+      <h2 style={{fontWeight: 'normal'}}>
+        Welcome to Reframe
+      </h2>
+      <Text style={{ fontSize: 80, textAlign: 'right' }} >
+        Home 
+      </Text>
     </div>
-);
+  </div>
+)
 
 const WelcomePage = {
     route: '/',
     view: Welcome,
 
-    title: 'Welcome', // <title>
+    title: 'Уроки React Native', // <title>
     description: 'This is a Reframe App.', // <meta name="description">
 
     // The landing page doesn't contain any interactive views.
